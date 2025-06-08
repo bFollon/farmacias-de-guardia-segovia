7eb12ffad9adb788543b7150d5af6107fb316a7f
Bruno Follon <follon.bruno@gmail.com>
2025-06-08 02:16:07 +0200

    Initial commit

---
+++ FarmaciasDeGuardiaTests/FarmaciasDeGuardiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FarmaciasDeGuardiaTests.swift
+//  FarmaciasDeGuardiaTests
+//
+//  Created by Bruno Follon on 16/5/25.
+//
+
+import Testing
+@testable import FarmaciasDeGuardia
+
+struct FarmaciasDeGuardiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
