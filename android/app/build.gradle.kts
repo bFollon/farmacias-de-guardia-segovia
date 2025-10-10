ddbaa8602a621952d10640e6a1a74f18ad451d93
Bruno Follon <follon.bruno@gmail.com>
2025-10-10 22:28:19 +0200

    Updates app version
    Increments the version code and updates the version name.
    
    This prepares the application for release.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.bfollon.farmaciasdeguardiaensegovia"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.0"
+        versionCode = 10
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
