d7d1f4f7d05f5708f88da32cfd89d8a2727ef457
Bruno Follon <follon.bruno@gmail.com>
2025-12-14 01:36:42 +0100

    Updates app version and adds padding
    Increments the app's version code and name.
    
    Adds horizontal padding to the Cantalejo info screen for improved readability.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.bfollon.farmaciasdeguardiaensegovia"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "1.1.13"
+        versionCode = 28
+        versionName = "1.1.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
